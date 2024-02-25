107b521a8690a9c7922b737abf6b41f8cf99690b
rickysilitonga112 <rickysilitonga112@gmail.com>
2024-02-25 17:25:27 +0700

    Initial Commit

---
+++ MobileAndon/MobileAndonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAndonApp.swift
+//  MobileAndon
+//
+//  Created by Ricky Silitonga on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileAndonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
